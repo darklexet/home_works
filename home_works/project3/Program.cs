611d995574ea474df1bfc338f58e0ae9a29f43ac
darklexet <lexet77@mail.ru>
2023-04-21 20:52:14 +0700

    add project3

---
+++ home_works/project3/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Console.WriteLine("Enter a number to check for parity: ");
+int parity = Convert.ToInt32(Console.ReadLine());
+if(parity % 2 == 0) 
+{
+    Console.WriteLine("The entered number is even");
+}
+else 
+{
+        Console.WriteLine("The entered number is not even");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
